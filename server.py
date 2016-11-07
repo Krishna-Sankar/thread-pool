05393a72e52c25fe5e6ebfb1a46b7b4d8a28974b
Benoit Chabod <be.chabod@gmail.com>
2016-11-07 01:02:45 +0100

    Fix whitespaces and handle client disconnection

--- server.py
+++ server.py
@@ -1,27 +1,32 @@
-import socket
+import socket, errno
 from threading import Thread
 
 class Client(Thread):
     def __init__(self):
-        Thread.__init__(self) 
+        Thread.__init__(self)
 
     def run(self):
         while True:
             data = conn.recv(2048)
-            print "Server received data:", data
-            conn.send("pong")
+            if data != "":
+                print "Server received data:", data
+            try:
+                conn.send("pong")
+            except IOError as e:
+                print "Client unreachable"
+                break
 
-PORT = 3000 
+PORT = 3000
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind(("0.0.0.0", PORT))
-pool = [] 
+pool = []
 
 while True:
     # At most one queued client
     server.listen(1)
-    (conn, (ip,port)) = server.accept() 
+    (conn, (ip,port)) = server.accept()
     clientThread = Client()
     clientThread.start()
     pool.append(clientThread)
