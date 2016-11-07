a6e234dffa1895c8a671fd1d051b06ed344aa56e
Benoit Chabod <be.chabod@gmail.com>
2016-11-07 19:22:21 +0100

    Refactor server to implement fixed thread pool responding to messages and clean exit

--- server.py
+++ server.py
@@ -1,37 +1,110 @@
-import socket, errno
+import socket, time, threading, signal
 from threading import Thread
 
-class Client(Thread):
-    def __init__(self, conn):
+NB_THREADS = 5
+PORT = 3000
+
+class Pool():
+    def __init__(self):
+        self.lockBusy = threading.Lock()
+        self.lockClients = threading.Lock()
+        self.clients = []
+        self.workers = []
+        self.busy = 0
+        self.killRequested = False
+        for counter in range(NB_THREADS):
+            self.workers.append(Worker(self, counter))
+            self.workers[counter].start()
+
+    def assignClient(self, conn):
+        print "Pool has been asked to append a client to the task list"
+        self.lockClients.acquire()
+        self.clients.append(conn)
+        self.lockClients.release()
+
+    def kill(self):
+        self.killRequested = True
+
+class Server(Thread):
+    def __init__(self, pool):
         Thread.__init__(self)
-        self.conn = conn
+        self.daemon = True # This thread may die while waiting for a client
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server.bind(("0.0.0.0", PORT))
+        self.pool = pool
 
     def run(self):
         while True:
+            # At most 4 queued clients
+            self.server.listen(4)
+            (conn, (ip,port)) = self.server.accept()
+            print "Server received client connection"
+            self.pool.assignClient(conn)
+
+class Worker(Thread):
+    def __init__(self, pool, id):
+        Thread.__init__(self)
+        self.pool = pool
+        self.conn = None
+        self.id = id
+
+    def run(self):
+        while not self.pool.killRequested:
+            # Try to get a client
+            self.pool.lockClients.acquire()
+            if len(self.pool.clients) > 0:
+                self.conn = self.pool.clients.pop(0)
+            self.pool.lockClients.release()
+
+            # If we didn't get a client, try again
+            if self.conn is None:
+                continue
+
+            print "Thread {0} fetched a client".format(self.id)
+            # If we have a client, indicate pool that we're busy
+            self.pool.lockBusy.acquire()
+            self.pool.busy += 1
+            self.pool.lockBusy.release()
+
+            # Serve client
             data = self.conn.recv(2048)
-            if data != "":
-                print "Server received data:", data
-            try:
-                self.conn.send("pong")
-            except IOError as e:
-                print "Client unreachable"
-                break
+            print "Thread {0} received data {1}".format(self.id, data)
+            if data == "KILL_SERVICE\n":
+                self.pool.kill()
+            elif data == "HELO text\n":
+                try:
+                    self.conn.send(defaultReply)
+                except IOError as e:
+                    # Client unreachable, nothing to be done
+                    pass
 
-PORT = 3000
+            self.conn.close()
+            self.conn = None
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server.bind(("0.0.0.0", PORT))
-pool = []
+            # Indicate pool that we're done
+            self.pool.lockBusy.acquire()
+            self.pool.busy -= 1
+            self.pool.lockBusy.release()
+        print "Thread {0} dying".format(self.id)
+
+print "--- Preparing thread pool..."
+workerPool = Pool()
+defaultReply = "HELO text\nIP:{0}\nPort:{1}\nStudentID:{2}\n".format(socket.gethostbyname(socket.gethostname()), PORT, 16336617)
+
+print "--- Creating CTRL-C signal handler..."
+def signalHandler(signal, frame):
+    print "Server received CTRL-C"
+    workerPool.kill()
+signal.signal(signal.SIGINT, signalHandler)
+
+print "--- TCP server starting..."
+serverThread = Server(workerPool)
+serverThread.start()
+print "--- Server is ready!"
 
 while True:
-    # At most one queued client
-    server.listen(1)
-    (conn, (ip,port)) = server.accept()
-    clientThread = Client(conn)
-    clientThread.start()
-    pool.append(clientThread)
-
-# Wait for every thread to finish
-for thread in pool:
-    thread.join()
+    if workerPool.killRequested:
+        for worker in workerPool.workers:
+            worker.join()
+        break
