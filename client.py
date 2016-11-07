1d00358dc16750f84cf7b7606be5d33a8a787d4d
Benoit Chabod <be.chabod@gmail.com>
2016-11-07 19:21:34 +0100

    Update client to send HELO and KILL messages

--- client.py
+++ client.py
@@ -1,15 +1,28 @@
-import socket
+import socket, sys
+
+if (len(sys.argv) < 2):
+	print "Client usage: python client.py H|K"
+	sys.exit(0)
 
 HOST = socket.gethostname()
 PORT = 3000
 
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serverSocket.connect((HOST, PORT))
 
-while True:
-    serverSocket.send("ping")
-    data = serverSocket.recv(2048)
-    print "Client received data:", data
-    raw_input("Press any key to send other messages...")
+# Try to connect to server
+try:
+	serverSocket.connect((HOST, PORT))
+except IOError as e:
+	print "Server unreachable"
+	sys.exit(0)
 
+# Send appropriate message
+if (sys.argv[1]=="H"):
+	serverSocket.send("HELO text\n")
+	data = serverSocket.recv(2048)
+	print "Client received data:", data
+else:
+	serverSocket.send("KILL_SERVICE\n")
+
+#Close socket
 serverSocket.close()
